7792d90b2480fea898d88d380c9cfebaacf2428f
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-04-26 02:28:45 +0600

    change for deploy

--- src/Components/Shared/WarningModal/index.tsx
+++ src/Components/Shared/WarningModal/index.tsx
@@ -28,7 +28,7 @@ export default function WarningModal({open,handleClose}:{open:boolean,handleClos
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+             
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
